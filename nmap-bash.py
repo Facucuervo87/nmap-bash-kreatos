635f8056be6699425aa3ba217e59f46e29e46b89
Facucuervo87 <facucu@gmail.com>
2022-06-02 13:42:07 +0000

    Added function to scan ip:port just put on the target file a service like 127.0.0.1:80 and generate a bash with nmap -sSCV -vvv -Pn -p 80 127.0.0.1 -oA assessment-127.0.0.1-80

--- nmap-bash.py
+++ nmap-bash.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from pdb import line_prefix
 import pandas as pd
 import argparse
 
@@ -9,13 +10,17 @@ parser = argparse.ArgumentParser(description='CSV to NMAP BASH')
 
 parser.add_argument('-i', '--input', help='Input file', required=True)
 parser.add_argument('-a', '--assesment', help='Name of the Assesment to name the ouput bash files', required=True)
+parser.add_argument('-p', '--port', help='Input with ports -p yes')
 
 tcp = "nmap -sSCV -vvv -Pn --top-ports=2000 "
 udp = "nmap -sUCV -vvv -Pn --top-ports=20 "
+port_scan = str("nmap -sSCV -vvv -Pn -p ")
+
 
 scope_file = FILE_PATH + "/" + parser.parse_args().input
 assesment =  parser.parse_args().assesment
 output_folder = FILE_PATH
+
 
 
 def scan_tcp(target):
@@ -41,22 +46,39 @@ def scan_udp(target):
             bash.write(command)
             bash.write("\n")
 
-
+def port_scan(target):
+    output_file = assesment + "_port-scan.sh"
+    with open(output_file, "w") as bash:
+        for line in targets:
+            host = line.rstrip('\n')
+            host = line.split(":")
+            target = host[0].rstrip('\n')
+            port = host[1].rstrip('\n')
+            port_scan = "nmap -sSCV -vvv -Pn -p "
+            output_port_scan = " -oA " + target + "-" + port
+            command_port_scan = port_scan + port + " " + target + output_port_scan
+            command = command_port_scan.rstrip('\n')
+            print(command)
+            bash.write(command)
+            bash.write("\n")
 
 df = pd.read_csv(scope_file)
 
+if parser.parse_args().port == 'yes':
+    with open(scope_file, "r") as targets:
+        print(f"Targets: {targets}")
+        for line in targets:
+            line = line.strip()
+            line = line.split(",")
+            target = line[0]
+            port_scan(target)
 
 
-with open(scope_file, "r") as targets:
-    for line in targets:
-        line = line.strip()
-        line = line.split(",")
-        target = line[0]
-        scan_tcp(target)
-
-with open(scope_file, "r") as targets:
-    for line in targets:
-        line = line.strip()
-        line = line.split(",")
-        target = line[0]
-        scan_udp(target)
+else:
+    with open(scope_file, "r") as targets:
+        for line in targets:
+            line = line.strip()
+            line = line.split(",")
+            target = line[0]
+            scan_tcp(target)
+            scan_udp(target)
